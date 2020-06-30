b1958d502d3236c027a083f4514e0a6c252a2237
Eric <eric@commitchange.com>
2020-06-30 15:58:11 -0500

    WIP

--- app/javascript/stories/AmountPane.tsx
+++ app/javascript/stories/AmountPane.tsx
@@ -1,12 +1,12 @@
 // License: LGPL-3.0-or-later
-import * as React from 'react';
+import React from 'react';
 import { action } from '@storybook/addon-actions';
 import AmountPane from '../components/payment_wizard/AmountPane';
-import { Money } from '../common/money'
+import { Money } from '../common/money';
 
 export default { title: 'AmountPane' };
 
-export const withAmounts = () => <AmountPane currentAmount={null} 
-  amountOptions={[500, 1000, 2500, 5000, 10000, 25000, 50000, 100000].map((i) => Money.fromCents(i, 'usd'))}
-  setAmount={(i:Money) => action('setAmount')} setValid={(i) => i}
-/>
+export const withAmounts:() => JSX.Element = () => <AmountPane currentAmount={Money.fromCents( 1000, 'usd')}
+	amountOptions={[500, 1000, 2500, 5000, 10000, 25000, 50000].map((i) => Money.fromCents(i, 'usd'))}
+	setAmount={action('set-amount')} setValid={(i) => i}
+/>;
