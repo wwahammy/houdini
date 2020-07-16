0dd82b8da1b1dd3fca68e59164c16224d15fe2c6
Eric Schultz <eric+this+is+ridiculous@commitchange.com>
2020-07-16 16:09:13 -0500

    WIP

---
+++ app/javascript/components/transaction_wizard/WizardInitializer.tsx
@@ -0,0 +1,17 @@
+
+// License: LGPL-3.0-or-later
+import * as React from "react"
+
+
+interface IWizardInitializerProps {
+}
+
+
+function WizardInitializer(props:IWizardInitializerProps) : JSX.Element {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+}
+
+export default WizardInitializer
