e2581d4c57eb8c0a608980b29d2e699b657ce3cc
Eric Schultz <eric@commitchange.com>
2021-10-15 17:37:06 -0500

    Make p-retry more typed

--- javascripts/src/lib/p-retry.ts
+++ javascripts/src/lib/p-retry.ts
@@ -3,13 +3,13 @@
 const retry = require('retry');
 
 export class AbortError extends Error {
-    originalError:any
-	constructor(message:any) {
+	originalError: any
+	constructor(message: any) {
 		super();
 
 		if (message instanceof Error) {
 			this.originalError = message;
-			({message} = message);
+			({ message } = message);
 		} else {
 			this.originalError = new Error(message);
 			this.originalError.stack = this.stack;
@@ -20,7 +20,7 @@ export class AbortError extends Error {
 	}
 }
 
-const decorateErrorWithCounts = (error:any, attemptNumber:number, options:any) => {
+const decorateErrorWithCounts = (error: any, attemptNumber: number, options: any) => {
 	// Minus 1 from attemptNumber because the first attempt does not count as a retry
 	const retriesLeft = options.retries - (attemptNumber - 1);
 
@@ -29,47 +29,49 @@ const decorateErrorWithCounts = (error:any, attemptNumber:number, options:any) =
 	return error;
 };
 
-const pRetry = (input:any, options:any) => new Promise((resolve, reject) => {
-	options = {
-		onFailedAttempt: () => {},
-		retries: 10,
-		factor: 1,
-		...options
-	};
+function pRetry<T>(input: (attemptNumber: number) => Promise<T>, options: any): Promise<T> {
+	return new Promise((resolve, reject) => {
+		options = {
+			onFailedAttempt: () => { },
+			retries: 10,
+			factor: 1,
+			...options
+		};
 
-	const operation = retry.operation(options);
+		const operation = retry.operation(options);
 
-	operation.attempt(async (attemptNumber:any) => {
-		try {
-			resolve(await input(attemptNumber));
-		} catch (error) {
-			if (!(error instanceof Error)) {
-				reject(new TypeError(`Non-error was thrown: "${error}". You should only throw errors.`));
-				return;
-			}
-
-			if (error instanceof AbortError) {
-				operation.stop();
-				reject(error.originalError);
-			} else if (error instanceof TypeError) {
-				operation.stop();
-				reject(error);
-			} else {
-				decorateErrorWithCounts(error, attemptNumber, options);
-
-				try {
-					await options.onFailedAttempt(error);
-				} catch (error) {
-					reject(error);
+		operation.attempt(async (attemptNumber: any) => {
+			try {
+				resolve(await input(attemptNumber));
+			} catch (error) {
+				if (!(error instanceof Error)) {
+					reject(new TypeError(`Non-error was thrown: "${error}". You should only throw errors.`));
 					return;
 				}
 
-				if (!operation.retry(error)) {
-					reject(operation.mainError());
+				if (error instanceof AbortError) {
+					operation.stop();
+					reject(error.originalError);
+				} else if (error instanceof TypeError) {
+					operation.stop();
+					reject(error);
+				} else {
+					decorateErrorWithCounts(error, attemptNumber, options);
+
+					try {
+						await options.onFailedAttempt(error);
+					} catch (error) {
+						reject(error);
+						return;
+					}
+
+					if (!operation.retry(error)) {
+						reject(operation.mainError());
+					}
 				}
 			}
-		}
-	});
-});
+		})
+	})
+};
 
-export default pRetry;
+		export default pRetry;
