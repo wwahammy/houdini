21e1b14a6f7853abcae71c3be0ffe28327adbf3f
Eric Schultz <eric@commitchange.com>
2021-11-19 14:57:26 -0600

    Add Transaction model

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  describe 'validation' do
+    it {is_expected.to validate_presence_of(:supporter)}
+  end
+
+  describe 'houid is created' do
+    subject { Transaction.new(supporter:create(:supporter))}
+    it {is_expected.to have_attributes(houid: match_houid('trx'))}
+  end
+end
