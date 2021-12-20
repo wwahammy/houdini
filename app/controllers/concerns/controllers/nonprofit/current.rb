a85d4470546a3cd618e82e6b16d29d89a4f9bbf5
Eric Schultz <eric@commitchange.com>
2021-12-20 15:32:49 -0600

    Add controller concerns

---
+++ app/controllers/concerns/controllers/nonprofit/current.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# License: AGPL-3.0-or-later WITH WTO-AP-3.0-or-later
+# Full license explanation at https://github.com/houdiniproject/houdini/blob/main/LICENSE
+module Controllers::Nonprofit::Current
+	extend ActiveSupport::Concern
+	included do
+		private
+
+		def current_nonprofit
+			@nonprofit = current_nonprofit_without_exception
+			raise ActionController::RoutingError, 'Nonprofit not found' if @nonprofit.nil?
+
+			@nonprofit
+		end
+
+		def current_nonprofit_without_exception
+			FetchNonprofit.with_params params, administered_nonprofit
+		end
+	end
+end
